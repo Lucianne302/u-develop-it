c19692460bae2bb4fba09c2d5eeefed107cbc076
lucianne302 <lucianne21@comcast.net>
2020-10-24 00:07:51 -0400

    added operations

--- server.js
+++ server.js
@@ -17,11 +17,34 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// // GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+  // Delete a candidate
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result, this, this.changes);
+  });
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
   });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
